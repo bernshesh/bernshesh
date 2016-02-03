7de08199043c63cd0857d20b206a6bb825c7f8f1
Ben Schiess <bern.shesh@gmail.com>
2016-02-02 17:57:20 -0700

    This would be for if statements.

---
+++ bernshesh/Assets/If.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class If : MonoBehaviour 
+{
+	public int cool = 10;
+
+	void Start ()
+	{
+		if (cool > 0) {
+			Debug.Log ("Too Small");
+		} else if (cool < -10) {
+			Debug.Log ("Really Small");
+		} else {
+			Debug.Log ("perfect");
+		}
+	}
+}
