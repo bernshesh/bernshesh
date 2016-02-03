ebefd00be559e86422e53e9c44a5b3cb386b066d
Ben Schiess <bern.shesh@gmail.com>
2016-02-02 17:24:25 -0700

    Flyscript Syntax

---
+++ bernshesh/Assets/FlyScript1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyScript1 : MonoBehaviour 
+{
+	private float heightTest = 3f;
+	void Start ()
+	{
+		Debug(transform.position.x);
+		if(transform.position.x <= heightTest)
+		{
+			Debug.Log("I'm about to hit the ground!");
+			FlyAway ();
+		}
+		else if(transform.position.x >= heightTest)
+		{
+			Debug.Log("I'm flying!");
+		}
+	}
+	private void FlyAway()
+	{
+		Debug.Log("Fly Away!");
+		Vector3 newPosition = new Vector3(0,7,0);
+		transform.position = newPosition;
+	}
+}
